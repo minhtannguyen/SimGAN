bba4e7aa77fe07fff628e76ade0800aa3e65f563
minhtannguyen <mn15@rice.edu>
2017-07-11 17:09:52 -0700

    debug

--- sim-gan.py
+++ sim-gan.py
@@ -189,12 +189,17 @@ def discriminator_network(input_image_tensor):
     :param input_image_tensor: Input tensor corresponding to an image, either real or refined.
     :return: Output tensor that corresponds to the probability of whether an image is real or refined.
     """
-    x = layers.Convolution2D(96, 3, 3, border_mode='same', subsample=(2, 2), activation='relu')(input_image_tensor)
-    x = layers.Convolution2D(64, 3, 3, border_mode='same', subsample=(2, 2), activation='relu')(x)
+    x = layers.Convolution2D(96, 3, 3, border_mode='same', subsample=(2, 2), activation='relu',
+                             weights=[1e-2*np.ones([3, 3, img_channels, 96]), np.zeros(96)])(input_image_tensor)
+    x = layers.Convolution2D(64, 3, 3, border_mode='same', subsample=(2, 2), activation='relu',
+                             weights=[1e-2*np.ones([3, 3, 96, 64]), np.zeros(64)])(x)
     x = layers.MaxPooling2D(pool_size=(3, 3), border_mode='same', strides=(1, 1))(x)
-    x = layers.Convolution2D(32, 3, 3, border_mode='same', subsample=(1, 1), activation='relu')(x)
-    x = layers.Convolution2D(32, 1, 1, border_mode='same', subsample=(1, 1), activation='relu')(x)
-    x = layers.Convolution2D(2, 1, 1, border_mode='same', subsample=(1, 1), activation='relu')(x)
+    x = layers.Convolution2D(32, 3, 3, border_mode='same', subsample=(1, 1), activation='relu',
+                             weights=[1e-2*np.ones([3, 3, 64, 32]), np.zeros(32)])(x)
+    x = layers.Convolution2D(32, 1, 1, border_mode='same', subsample=(1, 1), activation='relu',
+                             weights=[1e-2*np.ones([1, 1, 32, 32]), np.zeros(32)])(x)
+    x = layers.Convolution2D(2, 1, 1, border_mode='same', subsample=(1, 1), activation='relu',
+                             weights=[1e-2 * np.ones([1, 1, 32, 2]), np.zeros(2)])(x)
 
     # here one feature map corresponds to `is_real` and the other to `is_refined`,
     # and the custom loss function is then `tf.nn.sparse_softmax_cross_entropy_with_logits`
